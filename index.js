f177ef47545ebeb725cf225ddb098e7c52a13ba3
Aklima777 <aklimaa773@gmail.com>
2025-12-15 03:32:55 +0600

    user role by users call and verify admin and club manager

--- index.js
+++ index.js
@@ -14,6 +14,38 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 app.use(express.json());
 app.use(cors());
 
+// 1.varify firbase token 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase_admin_token.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+// 2.verify firebase token 
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+      return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+  try {
+      const idToken = token.split(' ')[1];
+      const decoded = await admin.auth().verifyIdToken(idToken);
+      console.log('decoded in the token', decoded);
+      req.decoded_email = decoded.email;
+      next();
+  }
+  catch (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+
+}
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,12 +68,76 @@ const client = new MongoClient(uri, {
      const db = client.db('clubsphere');
      const userCollection = db.collection('users');
 
+
+    //  / verify admin 
+
+        const verifyAdmin = async (req, res, next) => {
+          const email = req.decoded_email;
+          const query = {email};
+          const user = await userCollection.findOne(query);
+
+          if (!user || !['admin'].includes(user.role)) {
+            return res.status(403).send({ message: 'forbidden access' });
+          }
+        
+          next();
+        };
+
+        //verifyClubManageer
+        const verifyClubManageer = async (req, res, next) => {
+          const email = req.decoded_email;
+          const query = {email};
+          const user = await userCollection.findOne(query);
+
+          if (!user || !['club-manager'].includes(user.role)) {
+            return res.status(403).send({ message: 'forbidden access' });
+          }
+        
+          next();
+        };
+
+
     //  users api 
+
+    //get all users list
+
+        // users related apis
+        app.get('/users',verifyFBToken, async (req, res) => {
+          const searchText = req.query.searchText;
+          const query = {};
+
+          if (searchText) {
+              query.$or = [
+                  { displayName: { $regex: searchText, $options: 'i' } },
+                  { email: { $regex: searchText, $options: 'i' } },
+              ]
+
+          }
+
+          const cursor = userCollection.find(query).sort({ createdAt: -1 }).limit(10);
+          const result = await cursor.toArray();
+          res.send(result);
+      });
+
+// users role for dashboard 
+app.get('/users/:email/role',verifyFBToken,  async (req, res) => {
+  const email = req.params.email;
+
+   if (email !== req.decoded_email) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+
+  const user = await userCollection.findOne({ email });
+   res.send({ role: user?.role || 'member' });
+});
+
+
+
 
     // create and add users 
      app.post('/users', async (req, res) => {
         const user = req.body;
-        user.role = 'user';
+        user.role = 'member';
         user.createdAt = new Date();
         const email = user.email;
         const userExists = await userCollection.findOne({ email })
@@ -53,6 +149,20 @@ const client = new MongoClient(uri, {
         const result = await userCollection.insertOne(user);
         res.send(result);
     })
+
+    //patch update users 
+    app.patch('/users/:id/role', verifyFBToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const roleInfo = req.body;
+      const query = { _id: new ObjectId(id) }
+      const updatedDoc = {
+          $set: {
+              role: roleInfo.role
+          }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc)
+      res.send(result);
+  })
 
 
 
